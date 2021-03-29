24ee52b40808b070f8c6dcea78ae17349e55988c
lux-coded <selakluka@gmail.com>
2021-03-29 21:02:00 +0200

    Added Search Bar styling.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,16 @@ import React from 'react';
 import '../styles/SearchBar.css';
 
 class SearchBar extends React.Component {
+  onHandleChange(event) {
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div className='SearchBar'>
-        <form className='search form'>
-          <input placeholder='Looking for a movie or a show?'></input>
+        <form onSubmit={this.onHandleChange} className='search-form'>
+          <input placeholder='Looking for a movie or a show?' className='search-input'></input>
+          <button type='submit' className='search-bar-button'>Hey</button>
         </form>
       </div>
     );
