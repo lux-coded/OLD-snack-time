31edb41f507d0550d0f3af09b549a6aac17909b9
lux-coded <selakluka@gmail.com>
2021-03-29 21:50:06 +0200

    Search api Progress.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import themoviedb from '../api/themoviedb.js';
 
 class SearchBar extends React.Component {
+  // state = { search: '' };
+
+  async onChangeHandler(e) {
+    e.preventDefault();
+    // this.setState({ search: e.target.value });
+
+    const response = await themoviedb.get(`/search/photos`, {
+      params: { query: e.target.value },
+    });
+
+    console.log(response);
+  }
+
   onSubmitHandler(event) {
     event.preventDefault();
   }
@@ -8,8 +22,10 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className='SearchBar'>
-        <form onSubmit={this.onSubmitHandler} className='search-form'>
-          <input placeholder='Looking for a movie or a show?' className='search-input'></input>
+        <form onSubmit={this.onChangeHandler} className='search-form'>
+          <input type='text' placeholder='Looking for a movie or a show?' /*value={this.state.search} onChange={this.onChangeHandler}*/ className='search-input'>
+
+          </input>
           <button className='search-bar-button'>Hey</button>
         </form>
       </div>
